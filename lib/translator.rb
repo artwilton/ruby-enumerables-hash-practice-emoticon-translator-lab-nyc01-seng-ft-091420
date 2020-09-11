024282258c13489232c0b632633c357dd77ec1fe
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-11 19:04:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,7 @@ require "yaml"# require modules here
 def load_library(file_path)
   emoticons = YAML.load_file(file_path).map { |emotion, symbol| [emotion, [[:english, symbol[0]]].to_h] }.to_h
   YAML.load_file(file_path).map { |emotion, symbol| emoticons[emotion][:japanese] = symbol[1] }
+  emoticons
 end
 
 def get_japanese_emoticon
