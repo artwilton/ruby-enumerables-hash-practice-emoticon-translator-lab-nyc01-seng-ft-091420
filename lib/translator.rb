ec5c1ba4031014ba1304c5d476b916ecfc458a21
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-11 20:07:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def get_japanese_emoticon(file_path, emoticon)
   final_message
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   load_library(file_path)
   if translation = load_library(file_path).find {|key, value| value[:japanese] == emoticon }
     translation[1][:english]
